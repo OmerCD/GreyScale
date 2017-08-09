f50c8081696ecc8d7cebf848429a8aece8f5fe5e
Soul244 <hasancangedik@hotmail.com>
2017-08-10 00:08:51 +0300

    Arayüz Güncellemesi V3.3

---
+++ AForge.Wpf/StartUpWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AForge.Wpf
+{
+    /// <summary>
+    /// Interaction logic for StartUpWindow.xaml
+    /// </summary>
+    public partial class StartUpWindow : Window
+    {
+        public StartUpWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in SerialPort.GetPortNames())
+            {
+                ComList.Items.Add(item);
+            }
+        }
+    }
+}
